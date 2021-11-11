d85624390a388e28926ac1bf0d72fd4eb32ae341
AlanSamperio <93891123+AlanSamperio@users.noreply.github.com>
2021-11-10 20:38:26 -0600

    Update ExamenU2.php

--- ExamenU2.php
+++ ExamenU2.php
@@ -1,7 +1,7 @@
 <?php
 $port = 1433;
-$serverName = "serverexamen.database.windows.net" . $port;
-$database = "EXAMEN";
+$serverName = "tcp:serverexamen.database.windows.net," . $port;
+$database = "ExamenU2 ";
 $userName = "Student";
 $password = "Pa55w.rd";
 
